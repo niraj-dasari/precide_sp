f4f4b0314a18c1c120c403417844095305ab7344
Niraj Dasari <94426363+niraj-dasari@users.noreply.github.com>
2022-02-03 13:35:07 +0530

    twitter api added

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, data, prediction, sentiment_analysis # import your app modules here
+from apps import home, data, prediction, sentiment_analysis, user # import your app modules here
 
 app = MultiApp()
 # Add all your application here
@@ -8,6 +8,7 @@ app.add_app("Home", home.app)
 app.add_app("prediction", prediction.app)
 app.add_app("Data", data.app)
 app.add_app("Sentiment Analysis", sentiment_analysis.app)
+app.add_app("Profile Analysis", user.app)
 
 
 # The main app
